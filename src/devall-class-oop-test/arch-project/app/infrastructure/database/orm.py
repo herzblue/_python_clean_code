5755477b619e6b363403c1be14e59b2135e1b2f7
herzblue <herzblue@gmail.com>
2024-07-15 22:29:03 +0900

    ADD: db, orm

---
+++ src/devall-class-oop-test/arch-project/app/infrastructure/database/orm.py
@@ -0,0 +1,13 @@
+from peewee import *
+
+db = SqliteDatabase('database.db')
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class UserModel(BaseModel):
+    name = CharField(unique=True)
+    
+    class Meta:
+        table_name = 'users'
--- src/devall-class-oop-test/arch-project/app/infrastructure/fastapi/main.py
+++ src/devall-class-oop-test/arch-project/app/infrastructure/fastapi/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.controller.user import signup
+from app.infrastructure.database.orm import db, UserModel
 import uvicorn
 
 def create_app():
@@ -12,7 +13,12 @@ def create_app():
     
     return app
 
+def init_db():
+    db.connect()
+    UserModel.create_table()
+
 if __name__ == "__main__":
+    init_db()
     app = create_app()
     uvicorn.run(app, host="0.0.0.0", port=8000)
     
