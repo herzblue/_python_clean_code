6a5f1ec484f11d7124d40a79e902da0bb66edbf6
박용규 (TPCG) <ygpark@tpcg.co.kr>
2024-10-01 16:01:32 +0900

    complete: 07-Pydantic

--- fastapi_complete_guide/07-Pydantic/main.py
+++ fastapi_complete_guide/07-Pydantic/main.py
@@ -14,6 +14,7 @@ class Item(BaseModel):
     description: str = Field(None, max_length=500)
     price: float = Field(..., ge=0)
     tax: float = None
+    
     # 1-2. decorator 변수 없으면 모든 변수 가져오기.
     @model_validator(mode='after')
     def tax_must_be_less_than_price(cls, values):
@@ -25,30 +26,32 @@ class Item(BaseModel):
 
         return values
     
+# 2. 명확히 Path, Query 파라미터 지정 가능
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, q: str, item: Item=None):
-# 2. 명확히 Path, Query 파라미터 지정 가능
-#async def update_item(item_id: int = Path(...), q: str = Query(...), item: Item=None):
+# async def update_item(item_id: int = Path(...), q: str = Query(...), item: Item=None):
     return {"item_id": item_id, "q": q, "item": item}
 
 # 3. Path, Query, Request Body(json)
 @app.put("/items_json/{item_id}")
 async def update_item_json(
-    # 3-1. Form 형식과 동일 -> Path(... or None, 검증 조건)
+    # 3-1. Form 형식과 동일 -> Path(... -> 필수 or None -> 없어도 가능, 검증 조건)
     item_id: int = Path(..., gt=0),
+    # 3-2. Query는 하나당 하나의 값만 가능
     q1: str = Query(None, max_length=50),
-    #q1: Annotated[str, Query(max_length=50)] = None
+    #q1: Annotated[str, Query(max_length=50)] = None,
     q2: str = Query(None, min_length=3),
-    #q2: Annotated[str, Query(min_length=50)] = None
+    #q2: Annotated[str, Query(min_length=3)] = None,
     item: Item = None
 ):
     return {"item_id": item_id, "q1": q1, "q2": q2, "item": item}
 
-# Path, Query, Form
+# 4. Path, Query, Form - Form 검증 적용
 @app.post("/items_form/{item_id}")
 async def update_item_form(
     item_id: int = Path(..., gt=0, title="The ID of the item to get"),
     q: str = Query(None, max_length=50),
+    # 4-0. Form 검증 적용
     name: str = Form(..., min_length=2, max_length=50),
     description: Annotated[str, Form(max_length=500)] = None,
     #description: str = Form(None, max_length=500),
@@ -59,7 +62,7 @@ async def update_item_form(
     return {"item_id": item_id, "q": q, "name": name, 
             "description": description, "price": price, "tax": tax}
 
-# Path, Query, Form을 @model_validator 적용. 
+# 4-1. Form validation 1 - try except 구문: Item객체에서 에러 발생하면 ValidationError에서 잡도록 하기
 @app.post("/items_form_01/{item_id}")
 async def update_item_form_01(
     item_id: int = Path(..., gt=0, title="The ID of the item to get"),
@@ -75,8 +78,12 @@ async def update_item_form_01(
         item = Item(name=name, description=description, price=price, tax=tax)
         return item
     except ValidationError as e:
+        # RequestValidationError: ValidationError는 FASTAPI 기본 핸들러가 처리 못하기 때문에 RequestValidationError로 변경
         raise RequestValidationError(e.errors())
-   
+
+
+# 4-2. Form validation 2 - 의존성 주입: Depends(함수) 사용
+# parse_user_form을 schemas/item_schema.py에 분리하고 import해서 사용 / 반복되는 코드를 사용
 def parse_user_form(
     name: str = Form(..., min_length=2, max_length=50),
     description: Annotated[str, Form(max_length=500)] = None,
@@ -99,6 +106,7 @@ def parse_user_form(
 async def update_item_form_02(
     item_id: int = Path(..., gt=0, title="The ID of the item to get"),
     q: str = Query(None, max_length=50),
+    # Depends는 사실 Form
     item: Item = Depends(parse_user_form)
 ):
     return {"item_id": item_id, "q": q, "item": item}
